459ba692b9317cd6ad2d9726d89646bdf4a901c5
yavorska-iryna <iryna.yavorska@alleninstitute.org>
2025-02-25 07:40:44 +0000

    changing id to name

--- code/utils.py
+++ code/utils.py
@@ -56,7 +56,7 @@ def get_zarr_path(metadata: dict, path_to: str = 'motion_energy') -> str:
     Construct the path for saving Zarr storage based on metadata.
 
     Args:
-        metadata (dict): A dictionary containing metadata such as 'mouse_id', 'camera_label', and 'data_asset_id'.
+        metadata (dict): A dictionary containing metadata such as 'mouse_id', 'camera_label', and 'data_asset_name'.
         path_to (str): Specifies the type of frames to be saved ('gray_frames' or 'motion_energy_frames').
 
     Returns:
@@ -76,7 +76,7 @@ def get_data_path(metadata: dict) -> str:
     Construct the path for data storage based on metadata.
 
     Args:
-        metadata (dict): A dictionary containing metadata such as 'mouse_id', 'camera_label', and 'data_asset_id'.
+        metadata (dict): A dictionary containing metadata such as 'mouse_id', 'camera_label', and 'data_asset_name'.
 
     Returns:
         str: Full path to the data storage folder.
@@ -95,13 +95,13 @@ def construct_zarr_folder(metadata: dict) -> str:
     Construct the folder name for Zarr storage based on metadata.
 
     Args:
-        metadata (dict): A dictionary containing 'mouse_id', 'camera_label', and 'data_asset_id'.
+        metadata (dict): A dictionary containing 'mouse_id', 'camera_label', and 'data_asset_name'.
 
     Returns:
         str: Constructed folder name.
     """
     try:
-        return f"{metadata['mouse_id']}_{metadata['camera_label']}_{metadata['data_asset_id']}"
+        return f"{metadata['mouse_id']}_{metadata['camera_label']}_{metadata['data_asset_name']}"
     except KeyError as e:
         raise KeyError(f"Missing required metadata field: {e}")
 
